1c95a60b393372fa4c225e56a5ec8c70fa65aad7
Łukasz Korecki <lukasz@korecki.me>
2019-10-24 16:36:12 +0000

    v1.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/utility-belt "1.2.0"
+(defproject nomnom/utility-belt "1.2.1"
   :description "Some of the tools you'll ever need to fight crime and write Clojure stuffs"
   :url "https://github.com/nomnom-insights/nomnom.utility-belt"
   :deploy-repositories {"clojars" {:sign-releases false
