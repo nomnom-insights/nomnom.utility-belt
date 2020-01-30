6b302a293f6827c919bfc70736c85c84ea0a03c6
Łukasz Korecki <lukasz@korecki.me>
2020-01-29 20:11:23 -0800

    Add more debugg and logging

--- src/utility_belt/lifecycle.clj
+++ src/utility_belt/lifecycle.clj
@@ -1,17 +1,22 @@
 (ns utility-belt.lifecycle
   (:require [clojure.tools.logging :as log]))
 
-(def ^:private hooks (atom {:shutdown-agents shutdown-agents}))
+(def hooks (atom {:shutdown-agents shutdown-agents}))
 
 (defn register-shutdown-hook
   "Add a function to run when the application *gracefully* shuts down.
   Useful for stopping the Component system or long running threads"
   [name hook-fn]
+  (log/infof "registered hook %s" name)
   (swap! hooks assoc name hook-fn))
 
-(defn- run-registered-hooks []
-  (for [[name hook-fn] @hooks]
-    (log/infof "shutdown-hook=%s" name (hook-fn))))
+(defn run-registered-hooks []
+  (mapv (fn [[name hook-fn]]
+          (try
+            (log/infof "shutdown-hook=%s" name (hook-fn))
+            (catch Exception err
+              (log/errorf err "shutdown hook=%s failed" name))))
+        @hooks))
 
 (defn install-shutdown-hooks!
   "Install the shutdown handler, which will run any registered shutdown hooks."
