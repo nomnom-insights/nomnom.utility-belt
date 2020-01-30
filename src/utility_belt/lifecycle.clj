7d9a6ffcf01f4d7512bb0ff835f454a70d996a37
Łukasz Korecki <lukasz@korecki.me>
2020-01-29 16:54:20 -0800

    New lifecycle namespace to DRY-up application setup (shutdown hooks etc)

---
+++ src/utility_belt/lifecycle.clj
@@ -0,0 +1,24 @@
+(ns utility-belt.lifecycle
+  (:require [clojure.tools.logging :as log]))
+
+(def ^:private hooks (atom {}))
+
+(defn register-shutdown-hook
+  "Add a function to run when the application *gracefully* shuts down.
+  Useful for stopping the Component system or long running threads"
+  [name hook-fn]
+  (swap! hooks assoc name hook-fn))
+
+(defn- run-registerd-hooks []
+  (for [[name hook-fn] @hooks]
+    (log/infof "shutdown-hook=%s" name (hook-fn))))
+
+(defn init-shutdown-hooks
+  "Install the shutdown handler, which will:
+  - run any registerd shutdown hooks
+  - call shutdown-agents"
+  []
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. ^Runnable (fn shutdown-hook-runner []
+                                         (run-registerd-hooks)
+                                         (shutdown-agents)))))
