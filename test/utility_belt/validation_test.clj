b1d0a4113bfc8732e2d6981ef7bae11979543797
Łukasz Korecki <lukasz@korecki.me>
2020-05-19 21:43:59 -0700

    Add a bullet-proof-ish email validation regex

---
+++ test/utility_belt/validation_test.clj
@@ -0,0 +1,23 @@
+(ns utility-belt.validation-test
+  (:require
+   [clojure.test :refer [is deftest testing]]
+   [utility-belt.validation :as validation]))
+
+
+(deftest email-validation-and-extraction
+  (testing "it finds valid/invalid adddresses"
+    (is (= ["test@example.com"
+            nil
+            nil
+            nil
+            "ok.lolling@then.example.com"
+            "ok_lolling-1@then.example"
+            "ok_lolling-1+cool-label_ok@then.example.com"]
+           (mapv #(re-matches validation/email-pattern %)
+                 ["test@example.com"
+                  "@example.com"
+                  "ok@example."
+                  "foo"
+                  "ok.lolling@then.example.com"
+                  "ok_lolling-1@then.example"
+                  "ok_lolling-1+cool-label_ok@then.example.com"])))))
