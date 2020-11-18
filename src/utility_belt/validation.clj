9361646c067c42f44720cbd69e73b60e441bfde6
MarketaAdamova <marketa@nomnom.it>
2020-11-18 22:29:39 +0100

    fix email pattern (#5)

--- src/utility_belt/validation.clj
+++ src/utility_belt/validation.clj
@@ -3,7 +3,7 @@
 
 ;; Kudos to helpful people on the internets, including SO and IRC
 (def email-pattern-string
-  "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+  #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
 
 
 (def email-pattern (re-pattern email-pattern-string))
