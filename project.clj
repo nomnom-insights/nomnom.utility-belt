818e4088fe4a3f6878cac75df1f79b57f19294f8
marketa <marketa.adamova.cz@gmail.com>
2020-11-18 22:29:03 +0100

    fix email pattern

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/utility-belt "1.3.0"
+(defproject nomnom/utility-belt "1.3.1"
   :description "Some of the tools you'll ever need to fight crime and write Clojure stuffs"
   :url "https://github.com/nomnom-insights/nomnom.utility-belt"
   :deploy-repositories {"clojars" {:sign-releases false
